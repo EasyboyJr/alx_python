fe48094740eb34bdc4b2c7fa9829a3b1f0b06257
George <georgeoluwasemilore1@gmail.com>
2023-07-18 19:35:22 +0100

    added a else statement

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -13,3 +13,5 @@ elif int(num) < 6:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, num))
 elif int(num) >= 5 and number > 0:
     print("Last digit of {} is {} and is greater than 5".format(number, num))
+else:
+    print("i dont know what to do with your input")
