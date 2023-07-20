eb4d913bd9a69621b18ccc5867af74fe56f23aa2
George <georgeoluwasemilore1@gmail.com>
2023-07-20 02:11:03 -0700

    wrote a function that print a fibonacci sequence to a nth term

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def fibonacci_sequence(n):
+    fibonacci_seq = [0, 1]
+    for i in range(2, n):
+        next_num = (fibonacci_seq[-1]) + (fibonacci_seq[-2])
+        fibonacci_seq.append(next_num)
+    if n == 1:
+        return [0]
+    elif n == 0:
+        return []
+    else:
+        return fibonacci_seq
+
+print(fibonacci_sequence(6))
+print(fibonacci_sequence(1))
+print(fibonacci_sequence(0))
+print(fibonacci_sequence(20))
+
