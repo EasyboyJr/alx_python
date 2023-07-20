179240ef32c3599f5d4035a9a5a491658adc110e
George <georgeoluwasemilore1@gmail.com>
2023-07-20 09:06:57 +0100

    convert to celsius function

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -3,8 +3,4 @@ def add(a, b):
     a: any
     b: any
     result = (a + b)
-<<<<<<< HEAD
     return result
-=======
-    print(result))
->>>>>>> 65d167b92468ae995b95e5ef3f208ec08ea8f7c6
--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,10 +1,5 @@
-
+#!/usr/bin/python3
 def convert_to_celsius(fahrenheit):
-    fahrenheit: 
-    celsius = 
+    fahrenheit: any
+    celsius = (5 / 9 * (fahrenheit - 32))
     return celsius
-
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
