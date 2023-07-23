370fec5b2dd19b5577856948f942c346ca981eef
George <georgeoluwasemilore1@gmail.com>
2023-07-23 11:33:49 -0700

    added a function

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -3,3 +3,8 @@ def convert_to_celsius(fahrenheit):
     fahrenheit: any
     celsius = (5 / 9 * (fahrenheit - 32))
     return celsius
+
+print(convert_to_celsius(100))
+print(convert_to_celsius(-40))
+print(convert_to_celsius(-459.67))
+print(convert_to_celsius(32))
