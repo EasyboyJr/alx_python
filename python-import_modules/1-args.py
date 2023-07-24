11fd61ef1406c4a7b6ce8fcc8f5a2dbad338828d
George <georgeoluwasemilore1@gmail.com>
2023-07-24 09:53:22 +0100

    made myscript compliant with the validator

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,22 +3,21 @@ import sys
 def main():
     arguments = sys.argv[1:]
     argument_number = len(arguments)
-
     print("{}".format(argument_number), end=" ")
     if argument_number == 1:
-        print("argument:", end=" ")
+        print("argument:")
     elif argument_number == 0:
         print("arguments",end="")
     else:
-        print("arguments:", end=" ")
-    
+        print("arguments:")
     if argument_number > 0:
         print()
     else:
         print(".", end="\n")
         return
-    
-    for num, arg in enumerate(arguments, 1):
-        print("{}: {}".format(num, arg))
+    for num in enumerate(arguments, 1):
+        print("{}: {}".format(num, arguments))
+
+        
 if __name__ == "__main__":
     main()
