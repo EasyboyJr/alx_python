714deb19a81f6039d896afc341842eb9bbdb44b5
George <georgeoluwasemilore1@gmail.com>
2023-07-24 10:06:11 +0100

    moving to the terminal

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,6 +3,7 @@ import sys
 def main():
     arguments = sys.argv[1:]
     argument_number = len(arguments)
+
     print("{}".format(argument_number), end=" ")
     if argument_number == 1:
         print("argument:")
@@ -10,7 +11,10 @@ def main():
         print("arguments",end="")
     else:
         print("arguments:")
+
     if argument_number > 0:
+        print()
+    else:
         print(".", end="\n")
         return
     for num, arg in enumerate(arguments, 1):
