ebf7e1c1f8abc89f780cccfbdde387b3551330eb
George <georgeoluwasemilore1@gmail.com>
2023-07-26 06:03:01 -0700

    modified my function

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 # declare a function that prints out a matrix in format
 def print_matrix_integer(matrix=[[]]):
-    # use a nested for loop to iterate over the matrix and each row
+    # if matrix is empty, just print an empty line and return 
+    if not matrix:
+        print()
+        return
+    # iterate over each row in the matrix
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
-        print(end="\n")
+        # if row is empty print a new line and skip to next row 
+        if not row:
+            print()
+            continue
+        # iterate over each element in row using enumerate
+        for i, num in enumerate(row):
+            # if it is the last element in row print element folowed by a new line 
+            if i == len(row) -1:
+                print("{:d}".format(num))
+            # else if it is not the last print element followed by a space
+            else:
+                print("{:d}".format(num), end=" ")
