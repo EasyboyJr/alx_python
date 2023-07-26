612750b55edd28b0c15aa740cc5205fb89b9cc3f
George <georgeoluwasemilore1@gmail.com>
2023-07-26 02:41:55 -0700

    removed test lines

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -6,15 +6,3 @@ def print_matrix_integer(matrix=[[]]):
         for num in row:
             print("{:d}".format(num), end=" ")
         print(end="\n")
-
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
