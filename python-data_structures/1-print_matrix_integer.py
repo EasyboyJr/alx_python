ff83b3eead249dbf5881dd2a124086616e2ac7b0
George <georgeoluwasemilore1@gmail.com>
2023-07-26 02:34:45 -0700

    added a function that prints out a matrix using format

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# declare a function that prints out a matrix in format
+def print_matrix_integer(matrix=[[]]):
+    # use a nested for loop to iterate over the matrix and each row
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
