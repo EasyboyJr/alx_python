a749aa6d0e9f7998462e6294753e698fb3169a79
George <georgeoluwasemilore1@gmail.com>
2023-07-27 10:45:37 -0700

    removed using a variable because it was not necessary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_key = a_dictionary[key] = value
-    return new_key
+    a_dictionary[key] = value
+    return a_dictionary
