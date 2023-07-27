7f034b7dd0051e0aca0153cadb10f5d58315ec5b
George <georgeoluwasemilore1@gmail.com>
2023-07-27 07:05:21 -0700

    added a function that returns the common element in 2 sets

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    # use the intertection() function to get common element
+    element = set_1.intersection(set_2)
+    # return common element
+    return element
