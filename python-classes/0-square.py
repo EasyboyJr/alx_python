ef4e4fdec9bbd83184b8ff1c267b55f0ddeb7a88
George <georgeoluwasemilore1@gmail.com>
2023-08-01 11:49:30 +0100

    added a class that defines a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        size (int): The size of the square
+    """
+    def __init__(self, size):
+        """
+        Sets the value of size
+        """
+        self.__size = size
