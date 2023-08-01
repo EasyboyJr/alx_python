31917416cbb110bbbdc369e513c0f8ca569d245f
George <georgeoluwasemilore1@gmail.com>
2023-08-01 21:45:19 +0100

    added my print method

---
+++ python-classes/4-square.py
@@ -0,0 +1,72 @@
+"""
+This module calculates the area of a square.
+Methods:
+    __init__: Initialize the object.
+    size: Sets the value of the size of square.
+    area: calculates the area of the square.
+"""
+class Square:
+    """
+    A class that defines a square.
+    Attributes:
+        size (int): The size of thr square
+    """
+    def __init__(self, size=0):
+        """
+        Attributes:
+            self.__size: sets the value for size
+        """
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size
+    """
+    size.setter: sets and validate the value of the size
+    """
+    @size.setter
+    def size(self, value):
+        """
+        method:
+            size: validates the value of the size.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    """
+    method:
+        area: calculates the area of the square.
+    """
+    def area(self):
+        return self.__size * self.__size
+    """
+    Method:
+        print: prints in stdout the square with the character #
+    """
+    def my_print(self):
+        """
+        prints prints in stdout the square with the character #
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
+
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
