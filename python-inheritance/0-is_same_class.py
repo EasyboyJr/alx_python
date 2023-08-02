3f3a0e6505b65845ab5f2f64c44c0807ea994ec5
George <georgeoluwasemilore1@gmail.com>
2023-08-02 16:43:54 +0100

    added a docstring

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,3 +1,11 @@
+"""
+A module that checks if an object is an instance of a class
+"""
 def is_same_class(obj, a_class):
+    """
+    Parameters:
+        obj: The object.
+        a_class: The class to be compared to.
+    """
     value = type(obj) is a_class
     return value
