2baa61fac9a0e7abcc2e20ec98e5e8a425a031bd
George <georgeoluwasemilore1@gmail.com>
2023-08-02 16:53:31 +0100

    added a module that checks if obj is an instance o

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,10 @@
+"""
+a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.
+"""
+def is_kind_of_class(obj, a_class):
+    """
+    Parameters:
+        obj: the object.
+        a_class: the class to vompare the obj to.
+    """
+    return isinstance(obj, a_class)
