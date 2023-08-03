88da906f649ee51bf6442872b639113b5393abef
Onifade George Oluwasemilore <113639398+EasyboyJr@users.noreply.github.com>
2023-08-03 14:07:08 +0100

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -28,12 +28,25 @@ class BaseGeometry:
         """
         self.name = name
         self.value = value
-
-    def integer_validator(self, name, value):
-        """
-        raises an tyoe error if noe valid.
-        """
+        
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
+        
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class: initialize
+    """
+    def __init__(self, width, height):
+        """
+        Parameters:
+            width: int
+            height: int
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
