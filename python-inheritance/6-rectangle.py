74c9cd2e73e848e3ce7adf0fa97e1d6cb0191a17
George <georgeoluwasemilore1@gmail.com>
2023-08-03 06:57:18 -0700

    added 6-rectangle script

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+A class BaseGeometry
+Method:
+    area: Raises an exeption.
+"""
+class BaseGeometry:
+    """
+    Do nothing: By passing pass.
+    """
+    def __dir__(self):
+        attributes = super().__dir__()
+        return [i for i in attributes if i != '__init_subclass__']
+    """
+    area: takes in no agument other than self.
+    """
+    def area(self):
+        """
+        Raises an exception.
+        """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """
+        validates the input.
+        Attributes:
+            name(string): the name string.
+            value(int): must be an integer greater than 0.
+        """
+        self.name = name
+        self.value = value
+        
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+"""
+Rectangle class.
+Method:
+    __init__: initialize.
+"""
+class Rectangle(BaseGeometry):
+    """
+    __init__: initialize
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
