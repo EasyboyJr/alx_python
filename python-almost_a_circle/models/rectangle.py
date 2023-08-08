bc78637889c6a69731c61757b2a6a52aff040427
George <georgeoluwasemilore1@gmail.com>
2023-08-08 01:08:06 +0100

    updated rectangle class by adding validation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,6 +32,10 @@ class Rectangle(Base):
     
     @width.setter
     def width(self, new_width):
+        if not isinstance(new_width, int):
+            raise TypeError("width must be an integer")
+        if new_width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = new_width
     
     @property
@@ -40,6 +44,10 @@ class Rectangle(Base):
     
     @height.setter
     def height(self, new_height):
+        if not isinstance(new_height, int):
+            raise TypeError("height must be an integer")
+        if new_height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = new_height
     
     @property
@@ -48,6 +56,10 @@ class Rectangle(Base):
     
     @x.setter
     def x(self, new_x):
+        if not isinstance(new_x, int):
+            raise TypeError("x must be an integer")
+        if new_x <= 0:
+            raise ValueError("x must be > 0")
         self.__x = new_x
     
     @property
@@ -56,4 +68,8 @@ class Rectangle(Base):
     
     @y.setter
     def y(self, new_y):
+        if not isinstance(new_y, int):
+            raise TypeError("y must be an integer")
+        if new_y <= 0:
+            raise ValueError("y must be > 0")
         self.__y = new_y
