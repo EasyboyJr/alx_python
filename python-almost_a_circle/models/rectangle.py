471f99439b717612660dfc9e2bd571c0969e55c0
George <georgeoluwasemilore1@gmail.com>
2023-08-08 02:46:36 +0100

    overrides the default __str__ method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,7 @@ class Rectangle(Base):
         """Display the rectangle using char #."""
         for i in range(self.__height):
             print("#" * self.__width)
+    
+    def __str__(self):
+        """Returns a formatted output."""
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
