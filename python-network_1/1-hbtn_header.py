4a27fc6a5e26d611b1b4286a5d3fab65dd7048a0
George <georgeoluwasemilore1@gmail.com>
2023-08-10 11:23:54 +0100

    test

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,26 +5,7 @@ and displays the value of the variable X-Request-Id in the response header
 import sys
 import requests
 
-def main():
-    """main function:checks for the url, if found, retrn thebx-id"""
-    if len(sys.argv) != 2:
-        print("Usage: 1-hbtn_header.py <URL>")
-        sys.exit(1)
-    
-    url = sys.argv[1]
-
-    try:
-        response = requests.get(url)
-        if response.statys_code == 200:
-            x_request_id = response.headers.get('X-Request-Id')
-            if x_request_id:
-                print("X-Request-Id: {}".format(x_request_id))
-            else:
-                print("X-Request-Id not found")
-        else:
-            print("Request failed with status code: {}".format(response.status_code))
-    except requests.exeptions.RequestExeption as e:
-        print("An error occured: {}".format(e))
-
-if __name__ == "__main__":
-    main()
+url = sys.argv[1]
+req = requests.get(url)
+r = req.headers['X-Requests-Id']
+print(r)
