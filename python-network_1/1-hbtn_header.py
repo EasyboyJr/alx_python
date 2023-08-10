fdfc5d21cc1c70d960bf03dba7055c81e78a7835
George <georgeoluwasemilore1@gmail.com>
2023-08-10 11:26:54 +0100

    testing

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,7 +5,11 @@ and displays the value of the variable X-Request-Id in the response header
 import sys
 import requests
 
-url = sys.argv[1]
-req = requests.get(url)
-r = req.headers['X-Requests-Id']
-print(r)
+def main():
+    url = sys.argv[1]
+    req = requests.get(url)
+    r = req.headers['X-Requests-Id']
+    print(r)
+
+if __name__ =="__main__":
+    main()
