6e13660a7fb3984feba2d06b643becfedc3e8dd1
George <georgeoluwasemilore1@gmail.com>
2023-08-10 08:05:10 +0100

    removed

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import requests
-# req = requests.get('https://alu-intranet.hbtn.io/status')
-# print(req)
-print(requests.__version__)
-=======
 """
 A module that request a URL.
 """
@@ -13,4 +7,3 @@ req = requests.get('https://alu-intranet.hbtn.io/status')
 print("Body response:")
 print("\t- type:", type(req.text))
 print("\t- content:", req.text)
->>>>>>> 2bf429e2e0b936d31076e69a390ccbf663358937
