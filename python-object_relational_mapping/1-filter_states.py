473f9889cdaf25c3a2cbbcc442c7336a84573d46
George <georgeoluwasemilore1@gmail.com>
2023-08-22 02:03:26 +0100

    removed whitespace

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    
+
     # connect to MySQL
     database = MySQLdb.connect(host='localhost',
                                port=3306,
                                user=user,
                                passwd=password,
                                db=db_name)
-    
+
     # create a cursor
     cursor = database.cursor()
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     for item in result:
         print(item)
-    
+
     # close connections
     cursor.close()
     database.close()
