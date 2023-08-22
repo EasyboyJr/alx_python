3cd1db1863fc8fd97e4f28692578352029e44804
George <georgeoluwasemilore1@gmail.com>
2023-08-22 13:43:58 +0100

    a script that list all cities of s state by user

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,46 @@
+"""
+A script that takes in the name of a state as an argument
+and lists all cities of that state, using the database
+"""
+# import needed modules
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get details from user arguments.
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # connect to MySQL
+    database = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db_name
+    )
+
+    # create cursor
+    cursor = database.cursor()
+
+    # executing main task
+    main_query = (
+        """SELECT cities.id, cities.name, states.name
+        FROM cities INNER JOIN states
+        ON cities.state_id = state.id
+        WHERE states.name = %(state_key)s
+        ORDER BY cities.id ASC"""
+    )
+    cursor.execute(main_query, {'state_key': state_name})
+
+    # return results
+    result = cursor.fetchall()
+
+    for item in result:
+        print(result)
+    
+    # close connections
+    cursor.close()
+    database.close()
