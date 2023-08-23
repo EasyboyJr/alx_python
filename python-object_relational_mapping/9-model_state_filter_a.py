6eeac7e7c10f777915524e87d344898d2b43ee49
George <georgeoluwasemilore1@gmail.com>
2023-08-23 16:35:21 +0100

    test

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,14 +6,14 @@ Database entered by user.
 # import needed modules
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+import sys
 from model_state import Base, State
 
 if __name__ == "__main__":
     # get database details from user
-    user = argv[1]
-    password = argv[2]
-    db_name = argv[3]
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
     # connect to the database using create engine
     database = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user,
@@ -26,10 +26,11 @@ if __name__ == "__main__":
     session = Session
 
     # execute query
-    main_query = session.query(State).filter(State.name.contains('a')).order_by(State.id).all()
+    main_query = session.query(State).filter(State.name.contains('a'))\
+        .order_by(State.id).all()
 
-    for state in main_query:
-        print("{}: {}".format(state.id, state.name))
+    for item in main_query:
+        print("{}: {}".format(item.id, item.name))
 
     # close session
     session.close()
