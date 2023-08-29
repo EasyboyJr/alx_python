61f951888fe450b360259ddd87673ef816b6e203
George <georgeoluwasemilore1@gmail.com>
2023-08-29 13:20:59 +0100

    made my script return a 404 if validation fails

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -39,6 +39,8 @@ def magic(text):
 def num(n):
     if n.isdigit:
         return "{} is a number".format(n)
+    else:
+        return "404"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
