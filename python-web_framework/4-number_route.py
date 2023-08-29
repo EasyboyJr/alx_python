6cddbf13d437eecfac47856f999dcef3a7febe7b
George <georgeoluwasemilore1@gmail.com>
2023-08-29 13:18:45 +0100

    added a string

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -38,7 +38,7 @@ def magic(text):
 @app.route("/number/<n>", strict_slashes=False)
 def num(n):
     if n.isdigit:
-        return "{}".format(n)
+        return "{} is a number".format(n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
