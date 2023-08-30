0450302ad5d75dd2b04f53b4f461bc9cda18765c
George <georgeoluwasemilore1@gmail.com>
2023-08-30 11:19:46 +0100

    fixed my error

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -35,12 +35,9 @@ def magic(text):
     new_text = text.replace("_", " ")
     return "Python {}".format(new_text)
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def num(n):
-    if n.isdigit:
         return "{} is a number".format(n)
-    else:
-        abort(404)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
