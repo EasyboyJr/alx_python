e27527fbe6c173e05ef8bfbd193ca00b8ceb930d
Onifade George Oluwasemilore <113639398+EasyboyJr@users.noreply.github.com>
2023-09-02 13:53:59 +0100

    Update 2-temperature.py

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def convert_to_celsius(fahrenheit):
-<<<<<<< HEAD
     fahrenheit: any
     celsius = (5 / 9 * (fahrenheit - 32))
     return celsius
-
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
-=======
-    celsius = ((fahrenheit - 32) * 5 / 9)
-    return celsius
->>>>>>> c7055d09ad3facf21ce45a97b1f439e67e33a9b2
