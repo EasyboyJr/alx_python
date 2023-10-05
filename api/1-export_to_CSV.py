e503f054986dea1211f5ca9f485a1c18b9e54530
George <georgeoluwasemilore1@gmail.com>
2023-10-05 14:13:43 +0100

    test

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,47 +1,26 @@
+#Gather Data From an API
+
 import csv
 import json
 import requests
 import sys
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <employee_id>")
-        return
-
-    employee_id = sys.argv[1]
-    
-    # Request employee details and todo list from the API
-    response_user = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    response_todos = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-    
-    if response_user.status_code == 200 and response_todos.status_code == 200:
-        user_data = json.loads(response_user.text)
-        todos_data = json.loads(response_todos.text)
-        
-        employee_name = user_data.get("name")
-        username = user_data.get('username')
-        total_tasks = len(todos_data)
-        completed_tasks = sum(1 for task in todos_data if task["completed"])
-        
-        print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-        
-        for task in todos_data:
-            if task["completed"]:
-                print(f"\t {task['title']}")
-        
-        # Export data in CSV
-        file_name = '{}.csv'.format(employee_id)
-        with open(file_name, mode='w', newline='',) as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-            # header
-            csv_writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
-
-            # write each csv row
-            for task in todos_data:
-                csv_writer.writerow([employee_id, username, task['completed'], task['title']])
-    else:
-        print("Failed to retrieve data. Please check the employee ID and API availability.")
-
 if __name__ == "__main__":
-    main()
+    employee_id = sys.argv[1]
+    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
+    data = api_request.text
+    pjson = json.loads(data)
+    data1 = api_request1.text
+    pjson1 = json.loads(data1)
+
+    #export data to csv data
+    filename = "{}.csv".format(employee_id)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
+        for item in pjson1:
+            user_id = employee_id
+            username = pjson['username']
+            task_completed_status = item['completed']
+            task_title = item['title']
+            writer.writerow([user_id, username, task_completed_status, task_title])
