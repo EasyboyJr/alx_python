d37b14e369b93df7ed26d903bdc81b253e81e48c
George <georgeoluwasemilore1@gmail.com>
2023-10-05 14:20:02 +0100

    added Python script to export data in the JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,51 +1,35 @@
-"""
-A Python script to export data in the JSON format.
-"""
+'''
+A python script that uses the REST API for a given employee ID
+returns information about his/her TODO list progress.
+'''
 
 import json
 import requests
 import sys
 
-def main():
-    employee_id = sys.argv[1]
-    
-    # Request employee details and todo list from the API
-    response_user = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    response_todos = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-    
-    if response_user.status_code == 200 and response_todos.status_code == 200:
-        user_data = json.loads(response_user.text)
-        todos_data = json.loads(response_todos.text)
-        
-        employee_name = user_data.get("name")
-        total_tasks = len(todos_data)
-        completed_tasks = sum(1 for task in todos_data if task["completed"])
-        
-        print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-        
-        for task in todos_data:
-            if task["completed"]:
-                print(f"\t {task['title']}")
-
-        # Create a dictionary in the required JSON format
-        user_json_data = {
-            "USER_ID": [
-                {
-                    "task": task["title"],
-                    "completed": task["completed"],
-                    "username": user_data["username"]
-                }
-                for task in todos_data
-            ]
-        }
-
-        # Save the JSON data to a file
-        file_name = f'{employee_id}.json'
-        with open(file_name, 'w') as json_file:
-            json.dump(user_json_data, json_file, indent=4)
-
-    else:
-        print("Failed to retrieve data")
-
 if __name__ == "__main__":
-    main()
+    employee_id = sys.argv[1]
+    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
+    data = api_request.text
+    pjson = json.loads(data)
+    data1 = api_request1.text
+    pjson1 = json.loads(data1)
+
+    name_info = pjson['username']
+
+    filename = "{}.json".format(employee_id)
+
+    # Create the dictionary structure
+    result = {
+        employee_id: [{
+            "task": item["title"],
+            "completed": item["completed"],
+            "username": name_info
+        } for item in pjson1]
+    }
+
+    # export data to json file
+
+    with open(filename, "w") as outfile:
+        json.dump(result, outfile)
